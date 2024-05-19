f9fc2f7d90f727279db5b7a576a138b0047b7807
alicele2710 <alicele2710@gmail.com>
2024-05-19 17:48:26 -0400

    fix router

--- routes/openAIRouter.js
+++ routes/openAIRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const isAuthenticated = require('../middlewares/isAuthenticated');
 const { openAIController } = require('../controllers/openAIController');
-const checkApiRequestLimit = require('../middlewares/checkAPIRequestLimit');
+const checkApiRequestLimit = require('../middlewares/checkApiRequestLimit');
 
 // console.log('register function:', register); // Add this log to check if register is defined
 const openAIRouter = express.Router();
